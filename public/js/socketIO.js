79894cd8b688ac2f3b4aec3079a6679ca70621b4
costi-tica <costi13tica@gmail.com>
2021-05-11 23:33:09 +0300

    added socket.io connection

---
+++ public/js/socketIO.js
@@ -0,0 +1,32 @@
+const socket = io('http://localhost:3000');
+
+socket.on('connection', () => {
+
+})
+
+// socket.on('message', data => {
+//     console.log(data)
+// })
+
+document.querySelector("#btn1").onclick = () => {
+    var input = 5;
+    gainRP(input);
+}
+
+document.querySelector("#btn2").onclick = () => {
+    var input = 10;
+    loseRP(input);
+}
+
+function gainRP(RP) {
+    socket.emit('gainRP', RP);
+    // console.log('ti au adaugat 5 RP')
+}
+
+function loseRP(RP) {
+    socket.emit('loseRP', RP);
+    // console.log('ti au scazut 10 RP')
+}
+
+
+
