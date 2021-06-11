e70a6c73755a9f4ca4b965d42e76065b737093d4
costi-tica <costi13tica@gmail.com>
2021-06-11 13:19:02 +0300

    backend poker logic

--- src/modules/manageRoialPointz.js
+++ src/modules/manageRoialPointz.js
@@ -1,41 +1,51 @@
 const db = require('../config/dbConnection').db
 
-async function gainRP(sessionId, rp) {
+async function gainRPbyUserId(userId, rp) {
+    db.query(`
+        UPDATE profile 
+        SET RoialPointz = RoialPointz + ?
+        WHERE UserId = ?`, [rp, userId], (error) => {
+
+        if (error) {
+            console.log(error);
+        }
+    })
+}
+
+async function gainRPbySession(sessionId, rp) {
     db.query(`SELECT data FROM sessions WHERE session_id = ?`, [sessionId], (error, result) => {
         if (error) {
             console.error(error);
         }
         var userId = JSON.parse(result[0].data).userLoggedIn;
 
-        db.query(`
-            UPDATE profile 
-            SET RoialPointz = RoialPointz + ?
-            WHERE UserId = ?`, [rp, userId], (error) => {
-    
-            if (error) {
-                console.log(error);
-            }
-        })
+        gainRPbyUserId(userId, rp)
     })
 }
 
-async function loseRP(sessionId, rp) {
+async function loseRPbyUserId(userId, rp) {
+    db.query(`
+        UPDATE profile 
+        SET RoialPointz = RoialPointz - ?
+        WHERE UserId = ?`, [rp, userId], (error) => {
+
+        if (error) {
+            console.log(error);
+        }
+    })
+}
+
+async function loseRPbySession(sessionId, rp) {
     db.query(`SELECT data FROM sessions WHERE session_id = ?`, [sessionId], (error, result) => {
         if (error) {
             console.error(error);
         }
         var userId = JSON.parse(result[0].data).userLoggedIn;
 
-        db.query(`
-            UPDATE profile 
-            SET RoialPointz = RoialPointz - ?
-            WHERE UserId = ?`, [rp, userId], (error) => {
-
-            if (error) {
-                console.log(error);
-            }
-        })
+        loseRPbyUserId(userId, rp)
     })
 }
 
-module.exports = { gainRP, loseRP }
+
+
+module.exports = { gainRPbySession, gainRPbyUserId, loseRPbySession, loseRPbyUserId }
