a0bba208d54c394717c24dcfe11b9052943b868f
Theodorulus <theo_tudorache@yahoo.com>
2021-06-12 18:01:12 +0300

    poker front-end-design

---
+++ public/js/home.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+   var div = document.getElementById("logout-li");
+   var btn = document.getElementById("logout-btn");
+   
+	/*
+   div.onmouseover = function(e) {
+		console.log(div)
+		console.log(btn)
+		btn.classList.add("hover-bcg");
+   };
+   btn.onmouseout = function(e) {
+      div.class = "default-class-name";
+   };*/
+};
